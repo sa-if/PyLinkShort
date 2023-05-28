8c01f8c52ed7ab348f7add358aa2cf9bc6e09f09
Saif Islam <saifislam23122005@gmail.com>
2023-05-28 18:02:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pyshorteners
 
-# prompt the user to enter a URL
-url = input("Enter a URL to shorten: ")
+# prompt the user to enter a list of URLs to shorten
+urls = input("Enter a list of URLs to shorten (separated by commas): ").split(",")
 
-# create a Shortener object
+# create a pyshorteners.Shortener object
 s = pyshorteners.Shortener()
 
-# shorten the URL
-short_url = s.tinyurl.short(url)
 
-# print the shortened URL
-print("Shortened URL:", short_url)
+# shorten each URL in the list using the chosen service
+short_urls = []
+for url in urls:
+    
+    short_url = s.tinyurl.short(url)
+    
+    short_urls.append(short_url)
+
+# print the shortened URLs
+print("Shortened URLs:")
+for short_url in short_urls:
+    print(short_url)
